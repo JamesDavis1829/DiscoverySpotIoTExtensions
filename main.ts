bcef5230b41dab1f37d82533771af6b6c67a9610
James Davis <jedwarddavis902@gmail.com>
2019-05-27 14:21:38 -0500

    updated the code

--- main.ts
+++ main.ts
@@ -4,6 +4,23 @@ namespace discoveryspot {
     let proxAddr = 0x13
     let tempAddr = 54
 
+    const IR_DEFAULT = 0x13
+    const IR_COMMAND = 0x80
+    const IR_PRODUCTID = 0x81
+    const IR_PROXRATE = 0x82
+    const IR_IRLED = 0x83
+    const IR_AMBIENTPARAMETER = 0x84
+    const IR_AMBIENTDATA = 0x85
+    const IR_PROXIMITYDATA = 0x87
+    const IR_INTCONTROL = 0x89
+    const IR_PROXINITYADJUST = 0x8A
+    const IR_INTSTAT = 0x8E
+    const IR_MODTIMING = 0x8F
+    const IR_MEASUREAMBIENT = 0x10
+    const IR_MEASUREPROXIMITY = 0x08
+    const IR_AMBIENTREADY = 0x40
+    const IR_PROXIMITYREADY = 0x20
+    const IR_AMBIENT_LUX_SCALE = 0.25
     //% block
     export function ReadTemperature(): number {
         let temp = 0
@@ -17,7 +34,7 @@ namespace discoveryspot {
         temp = pins.i2cReadNumber(54, NumberFormat.UInt32BE, false)
         temp = temp & 0x3FFFFFFF
         temp = temp * 0.00001525878
-        basic.pause(100)
+        basic.pause(10)
         return temp
     }
 
@@ -32,52 +49,78 @@ namespace discoveryspot {
         )
         basic.pause(5)
         humidity = pins.i2cReadNumber(54, NumberFormat.UInt16BE, false)
-        basic.pause(100)
+        basic.pause(10)
         return humidity
     }
 
-    //% block
-    export function InitProximitySensor(): boolean {
-        pins.i2cWriteNumber(proxAddr, 0x81, NumberFormat.UInt8BE, false)
-        let id = pins.i2cReadNumber(proxAddr, NumberFormat.UInt8BE)
-        if ((id & 0xF0) != 0x20) {
-            return false;
-        } else {
-            pins.i2cWriteNumber(proxAddr, 0x89, NumberFormat.UInt8BE)
-            pins.i2cWriteNumber(proxAddr, 0x08, NumberFormat.UInt8BE)
-            return true
-        }
-    }
-
     function readU8(addr: number, command: number): number {
-        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE)
+        control.waitMicros(10)
+        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE, false)
+        control.waitMicros(10)
         return pins.i2cReadNumber(addr, NumberFormat.UInt8BE)
     }
 
     function writeU8(addr: number, command: number, value: number) {
-        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE)
-        pins.i2cWriteNumber(addr, value, NumberFormat.UInt8BE)
+        control.waitMicros(10)
+        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE, false)
+        control.waitMicros(10)
+        pins.i2cWriteNumber(addr, value, NumberFormat.UInt8BE, false)
     }
 
     function readU16(addr: number, command: number): number {
-        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE)
+        control.waitMicros(10)
+        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE, false)
+        control.waitMicros(10)
         return pins.i2cReadNumber(addr, NumberFormat.UInt16BE)
     }
 
+    //% block
+    export function InitProximitySensor(): boolean {
+        let revision = readU8(IR_DEFAULT, IR_PRODUCTID)
+        if ((revision & 0xF0) != 0x20) {
+            return false
+        } else {
+            writeU8(IR_DEFAULT, IR_IRLED, 20)
+            let timing = readU8(IR_DEFAULT, IR_MODTIMING)
+            timing &= ~0b00011000
+            timing |= (0 << 3) & 0xFF
+            writeU8(IR_DEFAULT, IR_MODTIMING, timing)
+            writeU8(IR_DEFAULT, IR_INTCONTROL, 0x08)
+            return true
+        }
+    }
 
     //% block
     export function ReadProximity(): number {
-        pins.i2cWriteNumber(proxAddr, 0x8E, NumberFormat.UInt8BE)
-        let status = pins.i2cReadNumber(proxAddr, NumberFormat.UInt8BE)
-        status = status & ~0x80
+        let status = readU8(IR_DEFAULT, IR_INTSTAT)
+        status &= ~0x80
+        writeU8(IR_DEFAULT, IR_INTSTAT, status)
 
-        pins.i2cWriteNumber(proxAddr, 0x8E, NumberFormat.UInt8BE)
-        pins.i2cWriteNumber(proxAddr, status, NumberFormat.UInt8BE)
+        writeU8(IR_DEFAULT, IR_COMMAND, IR_MEASUREPROXIMITY)
+        for (let x = 0; x < 50; x++) {
+            basic.pause(1)
+            let result = readU8(IR_DEFAULT, IR_COMMAND)
+            if (result & IR_PROXIMITYREADY) {
+                return readU16(IR_DEFAULT, IR_PROXIMITYDATA)
+            }
+        }
+        return 0
+    }
 
-        pins.i2cWriteNumber(proxAddr, 0x80, NumberFormat.UInt8BE)
-        pins.i2cWriteNumber(proxAddr, 0x08, NumberFormat.UInt8BE)
-        basic.pause(100)
-        pins.i2cWriteNumber(proxAddr, 0x87, NumberFormat.Int8LE)
-        return pins.i2cReadNumber(proxAddr, NumberFormat.UInt16BE)
+    //% block
+    export function ReadLight(): number {
+        let status = readU8(IR_DEFAULT, IR_INTSTAT)
+        status &= ~0x80
+        writeU8(IR_DEFAULT, IR_INTSTAT, status)
+
+        writeU8(IR_DEFAULT, IR_COMMAND, IR_MEASUREAMBIENT)
+        for (let x = 0; x < 50; x++) {
+            basic.pause(1)
+            let result = readU8(IR_DEFAULT, IR_COMMAND)
+            if (result & IR_AMBIENTREADY) {
+                return readU16(IR_DEFAULT, IR_AMBIENTDATA)
+            }
+        }
+        return 0
     }
 } 
