91c56a3439c5c4b888974876ea455e92af4e46dc
James Davis <jedwarddavis902@gmail.com>
2019-05-27 16:04:40 -0500

    Added neopixel code. Does not work yet

--- main.ts
+++ main.ts
@@ -54,37 +54,35 @@ namespace discoveryspot {
     }
 
     function readU8(addr: number, command: number): number {
-        control.waitMicros(10)
+        basic.pause(1)
         pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE, false)
-        control.waitMicros(10)
         return pins.i2cReadNumber(addr, NumberFormat.UInt8BE)
     }
 
     function writeU8(addr: number, command: number, value: number) {
-        control.waitMicros(10)
-        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE, false)
-        control.waitMicros(10)
-        pins.i2cWriteNumber(addr, value, NumberFormat.UInt8BE, false)
+        basic.pause(1)
+        pins.i2cWriteNumber(addr, (command << 8) + value, NumberFormat.UInt16BE, false)
+        //pins.i2cWriteNumber(addr, value, NumberFormat.UInt8BE, false)
     }
 
     function readU16(addr: number, command: number): number {
-        control.waitMicros(10)
-        pins.i2cWriteNumber(addr, command, NumberFormat.UInt8BE, false)
-        control.waitMicros(10)
-        return pins.i2cReadNumber(addr, NumberFormat.UInt16BE)
+        let highByte = readU8(addr, command)
+        let lowByte = readU8(addr, command + 1)
+        return (highByte << 8) + lowByte
     }
 
     //% block
     export function InitProximitySensor(): boolean {
+        basic.pause(1000)
         let revision = readU8(IR_DEFAULT, IR_PRODUCTID)
         if ((revision & 0xF0) != 0x20) {
             return false
         } else {
             writeU8(IR_DEFAULT, IR_IRLED, 20)
-            let timing = readU8(IR_DEFAULT, IR_MODTIMING)
-            timing &= ~0b00011000
-            timing |= (0 << 3) & 0xFF
-            writeU8(IR_DEFAULT, IR_MODTIMING, timing)
+            // let timing = readU8(IR_DEFAULT, IR_MODTIMING)
+            // timing &= ~0b00011000
+            // timing |= (3 << 3) & 0xFF
+            // writeU8(IR_DEFAULT, IR_MODTIMING, timing)
             writeU8(IR_DEFAULT, IR_INTCONTROL, 0x08)
             return true
         }
@@ -122,5 +120,45 @@ namespace discoveryspot {
             }
         }
         return 0
+    }
+
+    function sendLED0() {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        control.waitMicros(0.36)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        control.waitMicros(0.81)
+    }
+
+    function sendLED1() {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        control.waitMicros(0.71)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        control.waitMicros(0.61)
+    }
+
+    function sendLEDReset() {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        control.waitMicros(100)
+    }
+
+    function shiftOut(numberToShift: number) {
+        let us_to_delay = 0
+        numberToShift &= 0xFF
+        for (let x = 0; x < 8; x++) {
+            if ((numberToShift & (0x01 << x)) > 1) {
+                sendLED1()
+            } else {
+                sendLED0()
+            }
+        }
+    }
+
+    //% block
+    export function SendLEDColor(redVal: number, greenVal: number, blueVal: number): void {
+        sendLEDReset()
+        for (let x = 0; x < 24; x++) {
+            sendLED0()
+        }
+        sendLEDReset()
     }
 } 
